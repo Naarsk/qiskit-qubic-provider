198513b404e0232006f581d31aef50e3d8aa5847
Leonardo Cruciani <50495467+Naarsk@users.noreply.github.com>
2021-05-09 21:07:47 +0200

    Update qubic_backend.py
    Added a "simulator" class.

--- qubic_backend.py
+++ qubic_backend.py
@@ -79,3 +79,60 @@ class QUBICDevice(Backend):
         job = qubic_job.QUBICJob(self, job_id, qobj=circuit)
         
         return job
+    
+class Simulator(Backend):
+
+    def __init__(self, provider):
+        configuration = {
+            'backend_name': 'qubic_simulator',
+            'backend_version': '1.0',
+            'simulator': True,
+            'local': True,
+            'coupling_map': None,
+            'description': 'Qubic device',
+            'basis_gates': ['rx', 'ry', 'rxx', 'ms'],
+            'memory': False,
+            'n_qubits': 16,
+            'conditional': False,
+            'max_shots': 200,
+            'max_experiments': 1,
+            'open_pulse': False,
+            'gates': [
+                {
+                    'name': 'TODO',
+                    'parameters': [],
+                    'qasm_def': 'TODO'
+                }
+            ]
+        }
+        super().__init__(
+            configuration=BackendConfiguration.from_dict(configuration),
+            provider=provider)
+
+    @classmethod
+    def _default_options(cls):
+        return Options(shots=100,ids='DefaultId')
+
+    @deprecate_arguments({'qobj': 'circuit'})
+    def run(self, circuit, **kwargs):
+        for kwarg in kwargs:
+            if kwarg != 'shots' or kwarg != 'id':
+                warnings.warn(
+                    "Option %s is not used by this backend" % kwarg,
+                    UserWarning, stacklevel=2)
+            
+        out_shots = kwargs.get('shots', self.options.shots)
+            
+        job_id= kwargs.get('id', self.options.ids)
+        
+        qubic_json = circuit_to_qubic.circuit_to_qubic(
+            circuit, shots=out_shots)[0]
+        header = {"JobId": job_id, "SDK": "qiskit"}
+        
+        f=open('FakePut.txt',"w")
+        f.write('{}\n {}\n'.format(header, qubic_json))
+        f.close()
+        
+        job = qubic_job.QUBICJob(self, job_id, qobj=circuit)
+        
+        return job
