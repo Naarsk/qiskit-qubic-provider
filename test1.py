0297c27b9f1416de408ce035e3e081637a8516b9
Jan Balewski <janstar1122@gmail.com>
2021-05-09 15:00:55 -0700

    ok

---
+++ test1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Created on Wed Apr 21 12:05:36 2021
+
+@author: Leonardo
+"""
+#Importing standard Qiskit libraries
+from qiskit import QuantumCircuit, transpile, assemble
+from qubic_provider import QUBICProvider
+from qubic_job import QUBICJob
+
+#set the provider
+provider = QUBICProvider()  
+
+#set the backend
+backend = provider.backends.qubic_backend
+
+#create the circuit
+qc = QuantumCircuit(2)   #create a new 2-qubit quantum circuit
+qc.h(0)                  #add an hadamard gate on the 1st qubit
+qc.cx(0,1)               #add a cnot gate on the 2 qubits
+qc.measure_all()
+
+#transpile
+trans_qc = transpile(qc, backend)
+#assemble
+qobj = assemble(trans_qc, shots=100, backend=backend)
+
+#create a job instance
+job = QUBICJob(backend,'TEST1', qobj=qobj)
+#submit the job for execution (aka print the FakePut.txt)
+job.submit()
